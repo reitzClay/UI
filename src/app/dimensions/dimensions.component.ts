5f7b7a143cc8806a24626ca9e86c7158b689ac73
clayton <reitzclayton@gmail.com>
2024-03-07 13:36:28 +0100

    UiBase

--- src/app/dimensions/dimensions.component.ts
+++ src/app/dimensions/dimensions.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dimensions',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './dimensions.component.html',
   styleUrl: './dimensions.component.css'
 })
-export class DimensionsComponent {
+export class DimensionsComponent implements OnInit {
+  
+    public title: string;
 
+    constructor(){
+      this.title = 'Dimensions'
+    }
+
+    ngOnInit(): void {
+        console.log('Componente de dimsions')
+    }
 }
